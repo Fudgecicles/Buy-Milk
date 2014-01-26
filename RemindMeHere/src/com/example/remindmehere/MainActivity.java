fe108937c80453adfe46b0c1a5c865316a7d5754
Mitsukizzy <ibenaven@usc.edu>
2014-01-26 00:28:02 -0800

    Added location finder

--- RemindMeHere/src/com/example/remindmehere/MainActivity.java
+++ RemindMeHere/src/com/example/remindmehere/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.remindmehere;
 
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	  private Double latituteField;
+	  private Double longitudeField;
+	  private LocationManager locationManager;
+	  private String provider;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +26,8 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		Button createBtn = (Button) findViewById(R.id.create_button);
-		Button loginBtn = (Button) findViewById(R.id.add_button);
+		Button locationBtn = (Button) findViewById(R.id.location_button);
+		
 	
 
 	createBtn.setOnClickListener(new View.OnClickListener() {	
@@ -27,8 +39,54 @@ public class MainActivity extends Activity {
 		   }
 		});
 	
+	locationBtn.setOnClickListener(new View.OnClickListener() {		
+		@Override
+		public void onClick(View v) {
+		    // Get the location manager
+		    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		    // Define the criteria how to select the locatioin provider -> use
+		    // default
+		    Criteria criteria = new Criteria();
+		    provider = locationManager.getBestProvider(criteria, false);
+		    Location location = locationManager.getLastKnownLocation(provider);
+
+		    // Initialize the location fields
+		    if (location != null) {
+		      System.out.println("Provider " + provider + " has been selected.");
+		      onLocationChanged(location);
+		    } else {
+		      latituteField  = -1.0;
+		      longitudeField = -1.0;
+		    }
+		  }
+	});
 	
 	}
+	  public void onLocationChanged(Location location) {
+	    Double lat = location.getLatitude();
+	    Double lng = location.getLongitude();
+	    latituteField = lat;
+	    longitudeField = lng;
+	    Toast.makeText(this, "Latitude: " + latituteField + " - Longitude: " + longitudeField,
+		        Toast.LENGTH_SHORT).show();
+	  }
+
+	  public void onStatusChanged(String provider, int status, Bundle extras) {
+	    // TODO Auto-generated method stub
+
+	  }
+
+	  public void onProviderEnabled(String provider) {
+	    Toast.makeText(this, "Enabled new provider " + provider,
+	        Toast.LENGTH_SHORT).show();
+
+	  }
+
+	  public void onProviderDisabled(String provider) {
+	    Toast.makeText(this, "Disabled provider " + provider,
+	        Toast.LENGTH_SHORT).show();
+	  }
+	
 	
 	
 	@Override
