09efb3f7bfce0bb166735f7d00be6aa607e1342d
Mitsukizzy <ibenaven@usc.edu>
2014-01-25 20:00:25 -0800

    fix manifest

--- RemindMeHere/src/com/example/remindmehere/MainActivity.java
+++ RemindMeHere/src/com/example/remindmehere/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends Activity {
 		@Override
 		   public void onClick(View v) 
 		   {
-			Intent intent = new Intent(MainActivity.this, HandleReminders.class);
+			Intent intent = new Intent(MainActivity.this, Notification.class);
 			startActivity(intent); //switch activities
 		   }
 		});
